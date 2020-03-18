6fd63c352f45912c1bd640ac970129bebb07a4c9
danieltran67 <danieltran150@gmail.com>
2020-03-17 19:44:07 -0700

    Bootstrap working

---
+++ archer.py
@@ -0,0 +1,4 @@
+from app import app
+#from app import manager
+"""This file runs the website. We separated it so no one touches it."""
+app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+baseDirectory = os.path.abspath(os.path.dirname(__file__))
+#locate database to be in same working __file__
+class Config(object):
+    secret_key = os.environ.get("secret_key") or "its a secret"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+    'sqlite:///db.sqlite3' + os.path.join(baseDirectory, 'archer.db') #locates database
+
+    SQLALCHEMY_TRACK_MODIFICATION= False #logs any changes
